20a2370bf68f66a62a4edefd2c872478fe776a58
Ross Phillips <really.fake@really.really.really.fake>
2013-08-06 18:13:54 +1200

    standardise folder structure

--- src/main/scala/nz/co/rossphillips/thumbnailer/Main.scala
+++ src/main/scala/nz/co/rossphillips/thumbnailer/Main.scala
@@ -16,18 +16,18 @@ object Main {
 		thumbnailer.addThumbnailer(new TextThumbnailer)
 		thumbnailer.addThumbnailer(new DocxThumbnailer)
 
-		createThumbnail("test/resources/test.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-		createThumbnail("test/resources/test.pdf", "application/pdf")
-		createThumbnail("test/resources/test.png", "image/png")
-		createThumbnail("test/resources/test.txt", "text/plain")
-		createThumbnail("test/resources/test.jpg", "image/jpeg")
+		createThumbnail("src/main/resources/test.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+		createThumbnail("src/main/resources/test.pdf", "application/pdf")
+		createThumbnail("src/main/resources/test.png", "image/png")
+		createThumbnail("src/main/resources/test.txt", "text/plain")
+		createThumbnail("src/main/resources/test.jpg", "image/jpeg")
 	}
 
 	def createThumbnail(inputFile: String, contentType: String) {
 		val start = new Date
 
 		val input = new FileInputStream(inputFile)
-		val outputFilename = inputFile.substring(inputFile.lastIndexOf('.') + 1)
+		val outputFilename = inputFile.substring(inputFile.lastIndexOf('/') + 1)
 		val output = new FileOutputStream(s"output/$outputFilename.png", false)
 		thumbnailer.generateThumbnail(input, output, contentType)
 		output.close
